29118e9796b8db0f3b8b46fa3b756bc5a8739875
tahmidmahdi <tahmid5550@gmail.com>
2021-04-02 09:26:26 +0600

    spinner is added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,21 +1,41 @@
 import React, { useEffect, useState } from 'react';
+import { Spinner } from 'react-bootstrap';
 import ProductsData from '../ProductsData/ProductsData';
 import './Home.css'
 
 const Home = () => {
     const [products, setProducts] = useState([])
+    const [spinner, setSpinner] = useState(true)
     useEffect(()=>{
         fetch(`https://cherry-crisp-87098.herokuapp.com/products`)
         .then(res => res.json())
-        .then(data => setProducts(data))
+        .then(data => {
+            setProducts(data)
+            setSpinner(false)    
+        })
+         
     },[])
-    console.log(products);
+    
+    console.log('spinner',spinner);
+
+    
+    
+    
     return (
-        <div className="container home mt-5 ">
-            {
-                products.map(pd => <ProductsData pd={pd}></ProductsData>)
+       <div>
+            { (spinner) && 
+               <div className="container mt-5" style={{textAlign: 'center'}}> <Spinner animation="border" /></div>
             }
-        </div>
+            { (!spinner) && 
+                <div className="container home mt-5 "> 
+              
+                {
+                    products.map(pd => <ProductsData pd={pd}></ProductsData>)
+                }
+                
+                </div>
+            }
+       </div>
     );
 };
 
